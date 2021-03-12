be0396e886b8b2e2cf10b13f9225496a38c8bd6f
takedawon <takedawon@naver.com>
2021-03-13 00:01:49 +0900

    BaseActivity 생성

--- app/src/main/java/com/lanic/brandi/MainActivity.kt
+++ app/src/main/java/com/lanic/brandi/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.lanic.brandi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lanic.brandi.base.BaseActivity
+import com.lanic.brandi.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+        layoutId = R.layout.activity_main
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
     }
 }
