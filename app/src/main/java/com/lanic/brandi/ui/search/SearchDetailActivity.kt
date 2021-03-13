a75cee4f9fece070ab414dc61055ee7fdbc1f9c8
라닉 <takedawon@naver.com>
2021-03-13 17:40:56 +0900

    아이템 클릭시 사진 출력 기능 추가

--- app/src/main/java/com/lanic/brandi/ui/search/SearchDetailActivity.kt
+++ app/src/main/java/com/lanic/brandi/ui/search/SearchDetailActivity.kt
@@ -1,9 +1,20 @@
 package com.lanic.brandi.ui.search
 
+import android.os.Bundle
+import androidx.navigation.NavArgs
+import androidx.navigation.navArgs
 import com.lanic.brandi.R
 import com.lanic.brandi.base.BaseActivity
 import com.lanic.brandi.databinding.ActivitySearchDetailBinding
 
 class SearchDetailActivity : BaseActivity<ActivitySearchDetailBinding>(
     layoutId = R.layout.activity_search_detail
-)
+) {
+
+    private val argument by navArgs<SearchDetailActivityArgs>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.item = argument.item
+    }
+}
